9198100895790629588528685b6a18955e847b82
Kassim <kassim@sirateck.com>
2015-11-02 09:53:03 +0100

    Module structure with initial payment method webkit (without API)

---
+++ app/code/community/Sirateck/Lemonway/Model/Method/Webkit.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Sirateck_Lemonway extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category       Sirateck
+ * @package        Sirateck_Lemonway
+ * @copyright      Copyright (c) 2015
+ * @license        http://opensource.org/licenses/mit-license.php MIT License
+ */
+/**
+ * Method  standard model
+ *
+ * @category    Sirateck
+ * @package     Sirateck_Lemonway
+ * @author Kassim Belghait kassim@sirateck.com
+ */
+ class Sirateck_Lemonway_Model_Method_Webkit extends Mage_Payment_Model_Method_Abstract{
+ 	
+ 	
+ 	protected $_code  = 'lemonway_webkit';
+ 	/*protected $_formBlockType = 'cashway/form_cashway';
+ 	protected $_infoBlockType = 'cashway/info_cashway';*/
+ 	
+ 	
+ }
