e2cdc48d000b4b7da09bd89b27a357cad85cf1e8
Dat <dpham@lemonway.com>
2016-12-16 17:55:39 +0100

    SLL Verifier

--- src/app/code/community/Sirateck/Lemonway/Model/Apikit/Kit.php
+++ src/app/code/community/Sirateck/Lemonway/Model/Apikit/Kit.php
@@ -270,7 +270,7 @@ class Sirateck_Lemonway_Model_Apikit_Kit{
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->getConfig()->getWebkitUrl()."?moneyintoken=".$moneyInToken.'&p='.urlencode($cssUrl).'&lang='.$language);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, !$this->getConfig()->isTestMode());
 
         $server_output = curl_exec ($ch);
         if(curl_errno($ch))
